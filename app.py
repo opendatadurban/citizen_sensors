f3c9979e99a00374ee225c00be8770273c8258b3
heikoheilgendorff <heikoheilgendorff@users.noreply.github.com>
2018-09-03 15:32:34 +0200

    added windspeed and direction, PM 10 and 25

--- app.py
+++ app.py
@@ -42,6 +42,20 @@ class Temperature(db.Model):
     def __repr__(self):
         return '<temp {}>'.format(self.id)
 
+class Humidity(db.Model):
+    """
+    Humidity storage.
+    """
+    __tablename__ = 'humidity'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    values = Column(ARRAY(Float), nullable=False)
+    times = Column(ARRAY(DateTime), nullable=False)
+    date = Column(Date, nullable=False)
+    sensor_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<humid {}>'.format(self.id)
 
 class WindSpeed(db.Model):
     """
@@ -50,7 +64,7 @@ class WindSpeed(db.Model):
     __tablename__ = 'windspeeds'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    value = Column(ARRAY(Float), nullable=False)
+    values = Column(ARRAY(Float), nullable=False)
     times = Column(ARRAY(DateTime), nullable=False)
     date = Column(Date, nullable=False)
     sensor_id = Column(String, nullable=False)
@@ -58,6 +72,65 @@ class WindSpeed(db.Model):
     def __repr__(self):
         return '<wind_speed {}>'.format(self.id)
 
+class WindDirection(db.Model):
+    """
+    Wind direction storage.
+    """
+    __tablename__ = 'winddirections'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    values = Column(ARRAY(Float), nullable=False)
+    times = Column(ARRAY(DateTime), nullable=False)
+    date = Column(Date, nullable=False)
+    sensor_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<wind_dir {}>'.format(self.id)
+
+class Gas(db.Model):
+    """
+    Gas storage.
+    """
+    __tablename__ = 'gases'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    values = Column(ARRAY(Float), nullable=False)
+    times = Column(ARRAY(DateTime), nullable=False)
+    date = Column(Date, nullable=False)
+    sensor_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<gas {}>'.format(self.id)
+
+class PM10(db.Model):
+    """
+    PM10 storage.
+    """
+    __tablename__ = 'pm10s'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    values = Column(ARRAY(Float), nullable=False)
+    times = Column(ARRAY(DateTime), nullable=False)
+    date = Column(Date, nullable=False)
+    sensor_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<pm_10 {}>'.format(self.id)
+
+class PM25(db.Model):
+    """
+    PM25 storage.
+    """
+    __tablename__ = 'pm25s'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    values = Column(ARRAY(Float), nullable=False)
+    times = Column(ARRAY(DateTime), nullable=False)
+    date = Column(Date, nullable=False)
+    sensor_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<pm_25 {}>'.format(self.id)
 
 @app.route('/')
 def hello_world():
@@ -69,20 +142,50 @@ def ewv5000():
 
     if request.method == 'POST':
         temp = Temperature()
-        rain = Rain()
+        wind_speed = WindSpeed()
+        wind_dir = WindDirection()
         gas = Gas()
-        humid = Humid()
+        humid = Humidity()
+        pm_10 = PM10()
+        pm_25 = PM25()
         data = request.get_json()
 
         temp.values = temp.values + list(data['temp'])
-        rain.value = data['rain']
-        gas.value = data['press']
-        humid.value = data['humid']
+        temp.times = temp.times + list(data['temp_time'])
+        temp.sensor_id = temp.sensor_id + list(data['ID'])
+
+        gas.values = gas.values + list(data['gas'])
+        gas.times = gas.times + list(data['gas_time'])
+        gas.sensor_id = gas.sensor_id + list(data['ID'])
+
+        humid.value = humid.values + list(data['humid'])
+        humid.times = humid.times + list(data['temp_time'])
+        humid.sensor_id = humid.sensor_id + list(data['ID'])
+
+        wind_speed.values = wind_speed.values + list(data['wind_speed'])
+        wind_speed.times = wind_speed.times + list(data['winds_time'])
+        wind_speed.sensor_id = wind_speed.sensor_id + list(data['ID'])
+
+        wind_dir.values = wind_dir.values + list(data['wind_dir'])
+        wind_dir.times = wind_dir.times + list(data['windd_time'])
+        wind_dir.sensor_id = wind_dir.sensor_id + list(data['ID'])
+
+        pm_10.values = pm_10.values + list(data['pm_10'])
+        pm_10.times = pm_10.times + list(data['dust_time'])
+        pm_10.sensor_id = pm_10.sensor_id + list(data['ID'])
+
+        pm_25.values = pm_25.values + list(data['pm_25'])
+        pm_25.times = pm_25.times + list(data['dust_time'])
+        pm_25.sensor_id = pm_25.sensor_id + list(data['ID'])
+
 
         db.session.add(temp)
-        db.session.add(rain)
+        db.session.add(wind_speed)
+        db.session.add(wind_dir)
         db.session.add(gas)
         db.session.add(humid)
+        db.session.add(pm_10)
+        db.session.add(pm_25)
         db.session.commit()
 
     return render_template('ewok.html')
